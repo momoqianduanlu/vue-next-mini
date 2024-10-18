65ebe5beb9de5c526857397feadfaa1a41458053
cgs <215966931@qq.com>
2024-10-18 08:15:57 +0800

    update：renderer 完成 ELEMENT 节点挂载

--- packages/runtime-core/src/renderer-me.ts
+++ packages/runtime-core/src/renderer-me.ts
@@ -41,6 +41,52 @@ export function createRender(options: RendererOptions) {
 }
 
 function baseCreateRenderer(options: RendererOptions): any {
+  /**
+   * 解构 options，获取所有的兼容性方法
+   */
+  const {
+    insert: hostInsert,
+    patchProp: hostPatchProp,
+    createElement: hostCreateElement,
+    setElementText: hostSetElementText,
+    remove: hostRemove,
+    createText: hostCreateText,
+    setText: hostSetText,
+    createComment: hostCreateComment
+  } = options
+
+  const processElement = (oldVNode, newVNode, container, anchor) => {
+    if (oldVNode == null) {
+      // 挂载操作
+      mountElement(newVNode, container, anchor)
+    } else {
+      // 更新操作
+    }
+  }
+
+  /**
+   * element 的挂载操作
+   */
+  const mountElement = (vnode, container, anchor) => {
+    const { type, props, shapeFlag } = vnode
+    // 1. 创建element
+    const el = (vnode.el = hostCreateElement(type)) 
+    // 2. 设置文本
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      hostSetElementText(el, vnode.children)
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      // 数组
+    }
+    // 3. 处理props
+    if (props) {
+      for (const key in props) {
+        hostPatchProp(el, key, null, props[key])
+      }
+    }
+    // 4. 插入dom
+    hostInsert(el, container, anchor)
+  }
+
   const patch = (oldVNode, newVNode, container, anchor = null) => {
     if (oldVNode === newVNode) {
       return
@@ -59,12 +105,14 @@ function baseCreateRenderer(options: RendererOptions): any {
         break
       default:
         if (shapeFlag & ShapeFlags.ELEMENT) {
-          //
+          // 挂载
+          processElement(oldVNode, newVNode, container, anchor)
         } else if (shapeFlag & ShapeFlags.COMPONENT) {
           // 组件
         }
-
+    }
   }
+
   const render = (vnode, container) => {
     if (vnode === null) {
       // TODO: 卸载
@@ -72,7 +120,7 @@ function baseCreateRenderer(options: RendererOptions): any {
       patch(container._vnode, vnode, container)
     }
   }
-  
+
   return {
     render
   }
